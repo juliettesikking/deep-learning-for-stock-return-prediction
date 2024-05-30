b13cedda56a108067252837ad2fb4893a6014a8b
Juliette Sikking <juliette@tsf-508-wpa-1-187.epfl.ch>
2024-05-30 15:50:11 +0200

    mup loss lr plot all widths

--- mupcheckcoord.py
+++ mupcheckcoord.py
@@ -14,41 +14,46 @@ class MyModel(nn.Module):
         self.output_layer = nn.Linear(width, 10)
 
     def forward(self, x):
-        x = x.view(-1, 784)  # Flatten the images
+        x = x.view(-1, 784)
         x = torch.relu(self.input_layer(x))
         x = self.output_layer(x)
         return x
 
-def train_model(learning_rate, width=128):
+def train_model(learning_rate, width):
     # Prepare MNIST dataset
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
     train_data = MNIST(root='./data', train=True, download=True, transform=transform)
     train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
 
+    # Initialize the model
     model = MyModel(width=width)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     loss_fn = torch.nn.CrossEntropyLoss()
 
+    # Training loop
     model.train()
-    for epoch in range(5):
-        total_loss = 0
-        for images, labels in train_loader:
-            optimizer.zero_grad()
-            outputs = model(images)
-            loss = loss_fn(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
-        average_loss = total_loss / len(train_loader)
+    total_loss = 0
+    for images, labels in train_loader:
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = loss_fn(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    average_loss = total_loss / len(train_loader)
     return average_loss
 
 learning_rates = np.logspace(-20, -10, num=11, base=2)
-losses = [train_model(lr) for lr in learning_rates]
+widths = [128, 256, 512, 1024, 2048, 4096]
 
 plt.figure(figsize=(10, 5))
-plt.plot(np.log2(learning_rates), losses, marker='o')
+for width in widths:
+    losses = [train_model(lr, width) for lr in learning_rates]
+    plt.plot(np.log2(learning_rates), losses, marker='o', label=f'Width {width}')
+
 plt.xlabel('log2(Learning Rate)')
 plt.ylabel('Training Loss')
-plt.title('Maximal Update Parametrization with Adam Optimizer for Width 128 on MNIST')
+plt.title('Maximal Update Parametrization with Adam Optimizer for Various Widths on MNIST')
+plt.legend()
 plt.grid(True)
 plt.show()
